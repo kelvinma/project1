20ed2e2d961fd6fe516e0750d969a8f061dc6626
kelvinma <me@kelvinma.com>
2015-10-15 14:58:02 -0400

    Consolidation of JS + minor styling

--- js/tttapi.js
+++ js/tttapi.js
@@ -107,158 +107,162 @@ tttapi.watchGame = function (id, token) {
 
 //$(document).ready(...
 $(function() {
-  // takes form element's children's attributes and creates 'data' object
-  // used in register login functions
-  var form2object = function(form) {
-    var data = {};
-    $(form).children().each(function(index, element) {
-      var type = $(this).attr('type');
-      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
-        data[$(this).attr('name')] = $(this).val();
-      }
-    });
-    return data;
-  };
+//   // takes form element's children's attributes and creates 'data' object
+//   // used in register login functions
+//   var form2object = function(form) {
+//     var data = {};
+//     $(form).children().each(function(index, element) {
+//       var type = $(this).attr('type');
+//       if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+//         data[$(this).attr('name')] = $(this).val();
+//       }
+//     });
+//     return data;
+//   };
 
-  // builds {<root> : formData} (root is a string)
-  var wrap = function wrap(root, formData) {
-    var wrapper = {};
-    wrapper[root] = formData;
-    return wrapper;
-  };
+//   // builds {<root> : formData} (root is a string)
+//   var wrap = function wrap(root, formData) {
+//     var wrapper = {};
+//     wrapper[root] = formData;
+//     return wrapper;
+//   };
 
-// refactor this to work with game board
-  var callback = function callback(error, data) {
-    if (error) {
-      console.error(error);
-      $('#result').val('status: ' + error.status + ', error: ' +error.error);
-      return;
-    }
-    $('#result').val(JSON.stringify(data, null, 4));
-  };
-
-
-  $('#register').on('submit', function(e) {
-    var credentials = wrap('credentials', form2object(this));
-    tttapi.register(credentials, callback);
-    e.preventDefault();
-  });
-
-  $('#login').on('submit', function(e) {
-    var credentials = wrap('credentials', form2object(this));
-    var cb = function cb(error, data) { // data is object with propertiy user with object with property token
-                                        // data is passed back from server
-      if (error) {
-        alert('Log-error');
-        return;
-      }
-      callback(null, data);
-      $('.token').val(data.user.token); // sets .token as data.user.token
-      alert('Successfully logged in!');
-    };
-    e.preventDefault();
-    tttapi.login(credentials, cb);
-  });
+// // refactor this to work with game board
+//   var callback = function callback(error, data) {
+//     if (error) {
+//       console.error(error);
+//       $('#result').val('status: ' + error.status + ', error: ' +error.error);
+//       return;
+//     }
+//     $('#result').val(JSON.stringify(data, null, 4));
+//   };
 
 
-// LOOK AT THE HTML
-  $('#list-games').on('submit', function(e) {
-    var token = $('.token').val(); // retrieves token from hidden value
-    e.preventDefault();
-    tttapi.listGames(token, callback); // passes token as first argument
-  });
+//   $('#register').on('submit', function(e) {
+//     var credentials = wrap('credentials', form2object(this));
+//     tttapi.register(credentials, callback);
+//     e.preventDefault();
+//   });
 
-  $('#create-game').on('submit', function(e) {
-    var token = $(this).children('[name="token"]').val();
-    e.preventDefault();
-    var setGameId = function(error, data){
-      if (error) {
-        alert('Cannot create game');
-        return;
-      }
-      $('.grid').data('game-id', data.game.id);
-      $('#session-number').html($('.grid').data('game-id'));
-    };
-    tttapi.createGame(token, setGameId);
-  });
-
-  $('#show-game').on('submit', function(e) {
-    var token = $('.token').val();
-    var id = $('#show-id').val();
-    e.preventDefault();
-
-    var drawBoard = function (error, data){
-      if (error) {
-        alert('Saved session does not exist!');
-        return;
-      }
-      var saveState = data.game.cells;
-      for (var i = 0; i < grid.length; i++) {
-        grid[i] = saveState[i];
-        var square = '#'+i;
-        $(square).html(grid[i]);
-        $('.grid').data('game-id', data.game.id);
-        $('#session-number').html($('.grid').data('game-id'))
-      }
-    };
-    tttapi.showGame(id, token, drawBoard);
-
-  });
-
-  $('#join-game').on('submit', function(e) {
-    var token = $('.token').val();
-    var id = $('#join-id').val();
-    e.preventDefault();
-    var drawBoard = function (error, data){
-      if (error) {
-        alert('Saved session does not exist!');
-        return;
-      }
-      var saveState = data.game.cells;
-      for (var i = 0; i < grid.length; i++) {
-        grid[i] = saveState[i];
-        var square = '#'+i;
-        $(square).html(grid[i]);
-        $('.grid').data('game-id', data.game.id);
-        $('#session-number').html($('.grid').data('game-id'))
-      }
-
-    tttapi.joinGame(id, token, drawBoard);
-  });
+//   $('#login').on('submit', function(e) {
+//     var credentials = wrap('credentials', form2object(this));
+//     var cb = function cb(error, data) { // data is object with propertiy user with object with property token
+//                                         // data is passed back from server
+//       if (error) {
+//         alert('Log-error');
+//         return;
+//       }
+//       callback(null, data);
+//       $('.token').val(data.user.token); // sets .token as data.user.token
+//       alert('Successfully logged in!');
+//     };
+//     e.preventDefault();
+//     tttapi.login(credentials, cb);
+//   });
 
 
-  $('.grid').on('click', '.open', function() {
-    var token = $('.token').val(); // sets value of var token to class .token
-    var id = $('.grid').data('game-id');
-    var index = $(this).attr('id'); // sets index
-    var value = $(this).html(); // sets 'X' or 'O'
-    var data = wrap('game', wrap('cell', {index: index, value: value})); // passes object with index and value
-    // e.preventDefault();
-    tttapi.markCell(id, data, token, callback);
-  });
+// // LOOK AT THE HTML
+//   $('#list-games').on('submit', function(e) {
+//     var token = $('.token').val(); // retrieves token from hidden value
+//     e.preventDefault();
+//     tttapi.listGames(token, callback); // passes token as first argument
+//   });
+
+//   $('#create-game').on('submit', function(e) {
+//     var token = $(this).children('[name="token"]').val();
+//     e.preventDefault();
+//     var setGameId = function(error, data){
+//       if (error) {
+//         alert('Cannot create game');
+//         return;
+//       }
+//       $('.grid').data('game-id', data.game.id);
+//       $('#session-number').html($('.grid').data('game-id'));
+//       $('.board').show('slow');
+//     };
+//     tttapi.createGame(token, setGameId);
+//   });
+
+//   $('#show-game').on('submit', function(e) {
+//     var token = $('.token').val();
+//     var id = $('#show-id').val();
+//     e.preventDefault();
+
+//     var drawBoard = function (error, data){
+//       if (error) {
+//         alert('Saved session does not exist!');
+//         return;
+//       }
+//       var saveState = data.game.cells;
+//       for (var i = 0; i < grid.length; i++) {
+//         grid[i] = saveState[i];
+//         var square = '#'+i;
+//         $(square).html(grid[i]);
+//         $('.grid').data('game-id', data.game.id);
+//         $('#session-number').html($('.grid').data('game-id'))
+//         $('.board').show('slow');
+//       }
+//     };
+//     tttapi.showGame(id, token, drawBoard);
+
+//   });
+
+//   $('#join-game').on('submit', function(e) {
+//     var token = $('.token').val();
+//     var id = $('#join-id').val();
+//     e.preventDefault();
+//     var drawBoard = function (error, data){
+//       if (error) {
+//         alert('Saved session does not exist!');
+//         return;
+//       }
+//       var saveState = data.game.cells;
+//       for (var i = 0; i < grid.length; i++) {
+//         grid[i] = saveState[i];
+//         var square = '#'+i;
+//         $(square).html(grid[i]);
+//         $('.grid').data('game-id', data.game.id);
+//         $('#session-number').html($('.grid').data('game-id'))
+//       }
+
+//     tttapi.joinGame(id, token, drawBoard);
+//   };
 
 
-  $('#watch-game').on('submit', function(e){
-    var token = $(this).children('[name="token"]').val();
-    var id = $('#watch-id').val();
-    e.preventDefault();
+//   $('.grid').on('click', '.open', function() {
+//     var token = $('.token').val(); // sets value of var token to class .token
+//     var id = $('.grid').data('game-id');
+//     var index = $(this).attr('id'); // sets index
+//     var value = $(this).html(); // sets 'X' or 'O'
+//     var data = wrap('game', wrap('cell', {index: index, value: value})); // passes object with index and value
+//     // e.preventDefault();
+//     tttapi.markCell(id, data, token, callback);
+//   });
 
-    var gameWatcher = tttapi.watchGame(id, token);
 
-    gameWatcher.on('change', function(data){
-      var parsedData = JSON.parse(data);
-      if (data.timeout) { //not an error
-        this.gameWatcher.close();
-        return console.warn(data.timeout);
-      }
-      var gameData = parsedData.game;
-      var cell = gameData.cell;
-      $('#watch-index').val(cell.index);
-      $('#watch-value').val(cell.value);
-    });
-    gameWatcher.on('error', function(e){
-      console.error('an error has occured with the stream', e);
-    });
-  });
+//   $('#watch-game').on('submit', function(e){
+//     var token = $(this).children('[name="token"]').val();
+//     var id = $('#watch-id').val();
+//     e.preventDefault();
+
+//     var gameWatcher = tttapi.watchGame(id, token);
+
+//     gameWatcher.on('change', function(data){
+//       var parsedData = JSON.parse(data);
+//       if (data.timeout) { //not an error
+//         this.gameWatcher.close();
+//         return console.warn(data.timeout);
+//       }
+//       var gameData = parsedData.game;
+//       var cell = gameData.cell;
+//       $('#watch-index').val(cell.index);
+//       $('#watch-value').val(cell.value);
+//     });
+//     gameWatcher.on('error', function(e){
+//       console.error('an error has occured with the stream', e);
+//     });
+//   });
+
+//   });
 
 });
